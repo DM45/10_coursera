d214f82a610a7d15aa15a1891ab245474c05d621
DM45 <mda.tomsk@gmail.com>
2017-09-11 21:58:38 +0700

    Исправление ошибок
    Убраны переменные с подчеркиванием впереди, исключена необходимость ввода пути для файла, функция get_course_info разбита на две и несколько упрощена

--- coursera.py
+++ coursera.py
@@ -18,55 +18,60 @@ def get_full_url_courses_list():
 
 
 def get_random_url_courses_list(url_courses_list):
-    number_to_select = 20
+    number_to_select = 2
     list_of_random_url_courses = random.sample(
         url_courses_list, number_to_select)
     return list_of_random_url_courses
 
 
-def get_course_info(list_of_random_url_courses):
-    courses_info = []
-    val_of_week_for_grammat = 1
+def get_courses_info(list_of_random_url_courses):
+    full_courses_info = []
     for elem in list_of_random_url_courses:
         url = elem
         req_data = requests.get(url)
-        soup = BeautifulSoup(req_data.text, "lxml")
+        full_courses_info.append(req_data.text)
+    return full_courses_info
+
+
+def get_nesessary_part_of_courses_info(courses_info):
+    nesessary_courses_info = []
+    for course in courses_info:
+        soup = BeautifulSoup(course, "lxml")
         courses_name = (soup.find("h2", {
             "class": "headline-4-text course-title"})).text
         language = (
                 soup.find("div", {"class": "rc-Language"})).text
         start_date = (soup.find("div", {
             "class": "startdate rc-StartDateString caption-text"})).text
-        weeks_count = len(soup.find_all("div", {
-            "class": "week-heading body-2-text"}))
-        if weeks_count > val_of_week_for_grammat:
-            duration = "{}{}".format(weeks_count, " weeks")
-        else:
-            duration = "{}{}".format(weeks_count, " week")
+        duration = "{}{}".format(len(soup.find_all("div", {
+            "class": "week-heading body-2-text"})), ' week(s)')
         try:
             rating = (soup.find("div", {
-            	"class": "ratings-text bt3-visible-xs"})).text
+                "class": "ratings-text bt3-visible-xs"})).text
         except AttributeError:
             rating = ""
-        courses_info.append([courses_name, language, start_date, duration, rating])
-    return courses_info
+        nesessary_courses_info.append(
+                [courses_name, language, start_date, duration, rating])
+    return nesessary_courses_info
 
 
-def output_courses_info_to_xlsx(courses_info, filepath):
+def output_courses_info_to_xlsx(courses_info):
+    filepath_to_save = 'courses_info.xlsx'
     wb = Workbook()
     ws = wb.active
     for row in courses_info:
         ws.append(row)
-    wb.save(filepath)
+    wb.save(filepath_to_save)
 
 
 if __name__ == '__main__':
-    _filepath_to_save = input("Enter filepath with filename for save file: ")
     print("Please wait")
-    _get_full_url_courses_list = get_full_url_courses_list()
-    _get_random_url_courses_list = get_random_url_courses_list(
-            _get_full_url_courses_list)
-    _get_course_info = get_course_info(_get_random_url_courses_list)
-    _output_courses_info_to_xlsx = output_courses_info_to_xlsx(
-            _get_course_info, _filepath_to_save)
+    full_url_courses_list = get_full_url_courses_list()
+    random_url_courses_list = get_random_url_courses_list(
+            full_url_courses_list)
+    full_courses_info = get_courses_info(random_url_courses_list)
+    nesessary_part_of_courses_info = get_nesessary_part_of_courses_info(
+            full_courses_info)
+    courses_info_to_xlsx = output_courses_info_to_xlsx(
+            nesessary_part_of_courses_info)
     print("Work is done!")
